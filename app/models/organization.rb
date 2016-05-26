1e11718222e6b068a78255b04b0697a2e8c96055
Matt Pindell <pindell.m@gmail.com>
2016-05-25 22:33:33 -0600

    Create Organization model and accompanying test (#4)

---
+++ app/models/organization.rb
@@ -0,0 +1,13 @@
+class Organization
+  attr_reader :login, :avatar_url
+
+  def initialize(organization_data)
+    @login      = organization_data[:login]
+    @avatar_url = organization_data[:avatar_url]
+  end
+
+  def html_url
+    "https://github.com/#{login}"
+  end
+
+end
