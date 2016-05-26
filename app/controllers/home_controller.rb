5da7ea4543aa3eb87c43a37e4616eaa35bb29b3d
Matt Pindell <pindell.m@gmail.com>
2016-05-26 11:14:18 -0600

    Add flash message and refactor Search into Poro

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,13 +8,21 @@ class HomeController < ApplicationController
   end
 
   def search
-    service = GithubService.new(session[:token])
-    terms   = params["q"]
-    results = service.search(terms)
-    @total  = results[:total_count]
-
-    @search_items = results[:items].map do |search_data|
-      OpenStruct.new(search_data)
+    if session[:token]
+      terms    = params["q"]
+      @results = Search.new(session[:token], terms)
+    else
+      flash[:notice] = "Please Login in order to Search!"
+      redirect_to root_path
     end
+
+    # service = GithubService.new(session[:token])
+    # terms   = params["q"]
+    # results = service.search(terms)
+    # @total  = results[:total_count]
+    #
+    # @search_items = results[:items].map do |search_data|
+    #   OpenStruct.new(search_data)
+    # end
   end
 end
