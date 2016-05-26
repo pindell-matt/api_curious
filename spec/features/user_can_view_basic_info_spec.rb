4a021864cdb908165d2bf2bc95e608187131dffb
Matt Pindell <pindell.m@gmail.com>
2016-05-26 09:28:32 -0600

    Show Page Refactor + Feature Test (#6)
    * Update spec to always return proper token
    
    * Feature test covers basic user info
    
    * Add tabs to features test
    
    * Update .gitignore to include coverage
    
    * Remove Test dir

---
+++ spec/features/user_can_view_basic_info_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "User can view their basic info" do
+  scenario "after logging in with Github" do
+    VCR.use_cassette('feature#basic') do
+      token = ENV["TEST_TOKEN"]
+      allow_any_instance_of(GithubOauthService).to receive(:get_access_token).and_return(token)
+
+      visit '/auth/github/callback'
+
+      within(".basic-user-info") do
+        expect(page).to have_content("Matt Pindell")
+        expect(page).to have_content("pindell-matt")
+        expect(page).to have_content("pindell.m@protonmail.com")
+        expect(page).to have_content("Joined on 2014-09-16T19:59:39Z")
+      end
+
+      within(".vcard-stats") do
+        expect(page).to have_content("20 Followers")
+        expect(page).to have_content("0 Starred")
+        expect(page).to have_content("18 Following")
+      end
+
+      within(".contributions") do
+        expect(page).to have_content("contributions in the last year")
+      end
+
+      within(".tabbable") do
+        expect(page).to have_selector('li', text: "Followed User Activity", visible: true)
+        expect(page).to have_selector('li', text: "Repositories", visible: true)
+        expect(page).to have_selector('li', text: "Public Activity", visible: true)
+      end
+    end
+  end
+
+end
