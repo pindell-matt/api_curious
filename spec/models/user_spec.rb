a02bd4b17ee7591ae95f62800e10e2fbab3d9268
Matt Pindell <pindell.m@gmail.com>
2016-05-25 20:20:18 -0600

    Add repos_hash test to GithubService test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,41 @@ describe User do
       expect(repo.html_url).to eq("https://github.com/jdliss/swift")
     end
   end
+
+  it "returns the starred repo count" do
+    VCR.use_cassette('user#starred_repos') do
+      user    = User.new(ENV['TEST_TOKEN'])
+      starred = user.starred
+
+      expect(starred).to eq(0)
+    end
+  end
+
+  it "returns the users' followers count" do
+    VCR.use_cassette('user#followers') do
+      user    = User.new(ENV['TEST_TOKEN'])
+      starred = user.followers
+
+      expect(starred).to eq(20)
+    end
+  end
+
+  it "returns the users' followed count" do
+    VCR.use_cassette('user#following') do
+      user    = User.new(ENV['TEST_TOKEN'])
+      starred = user.following
+
+      expect(starred).to eq(18)
+    end
+  end
+
+  it "returns the users' name" do
+    VCR.use_cassette('user#following') do
+      user    = User.new(ENV['TEST_TOKEN'])
+      starred = user.following
+
+      expect(starred).to eq(18)
+    end
+  end
+
 end
--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -1,14 +1,32 @@
 require 'rails_helper'
 
 describe GithubService do
-  it "returns data for a given user" do
+  it "returns user data for a given user" do
     VCR.use_cassette('github_service#user') do
       service = GithubService.new(ENV["TEST_TOKEN"])
       user    = service.user_hash
 
-      expect(user[:login]).to    eq("pindell-matt")
-      expect(user[:id]).to       eq(8797393)
-      expect(user[:html_url]).to eq("https://github.com/pindell-matt")
+      expect(user[:avatar_url]).to eq("https://avatars.githubusercontent.com/u/8797393?v=3")
+      expect(user[:name]).to       eq("Matt Pindell")
+      expect(user[:login]).to      eq("pindell-matt")
+      expect(user[:email]).to      eq("pindell.m@protonmail.com")
+      expect(user[:created_at]).to eq("2014-09-16T19:59:39Z")
+      expect(user[:followers]).to  eq(20)
+      expect(user[:following]).to  eq(18)
+      expect(user[:html_url]).to   eq("https://github.com/pindell-matt")
+    end
+  end
+
+  it "returns the repos data for a given user" do
+    VCR.use_cassette('github_service#user', record: :new_episodes) do
+      service = GithubService.new(ENV["TEST_TOKEN"])
+      repos   = service.repos_hash
+      repo    = repos.first
+
+      expect(repos.count).to      eq(48)
+      expect(repo[:id]).to        eq(56370165)
+      expect(repo[:name]).to      eq("swift")
+      expect(repo[:full_name]).to eq("jdliss/swift")
     end
   end
 end
